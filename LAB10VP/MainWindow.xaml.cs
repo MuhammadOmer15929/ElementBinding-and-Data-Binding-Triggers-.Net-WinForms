92a6e5af185fbb11d32a436a8768e25959db33e6
omer0 <omer0@SHADOW>
2022-12-09 11:54:11 +0500

    Add project files.

---
+++ LAB10VP/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB10VP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        employees emp = new employees { FirstName ="Muhammad" ,LastName="omer",Gender="male",Contact="123",ADD ="ATTOCK",WarehouseNo="123",WarehouseName="Pakistan" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = emp;
+        }
+        employees omer1;
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = Brushes.Green;
+            this.Resources["brushResource"]= brush;
+        }
+    }
+}
---
+++ LAB10VP/employees.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB10VP
+{
+
+    class employees
+    {
+        public string FirstName { set; get; }
+        public string   LastName { set; get; }
+        public string  Gender{ set; get; }
+        public string Contact { set; get; }
+        public string ADD { set; get; }
+        public string WarehouseNo { set; get; }
+        public string WarehouseName { set; get; }
+
+
+
+    }
+}
